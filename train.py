d651fcf396e430ab712496c2f4d063372d6dca3c
Rauf <goodraff@gmail.com>
2020-02-10 13:16:19 +0000

    add history plot

--- train.py
+++ train.py
@@ -18,13 +18,63 @@ import os
 def generator_wrapper(generator):
     n_classes = [168, 11, 7]
     for batch_x, batch_y in generator:
-        # print([to_categorical(batch_y[:,i], num_classes=n_classes[i]) for i in range(3)])
         yield (batch_x,[to_categorical(batch_y[:,i], num_classes=n_classes[i]) for i in range(3)])
 
+def plot_param(history, save_path, param='', metric='loss'):
+    name = '{}_{}'.format(param, metric) if len(param)>0 else metric
+    
+    plt.plot(history.history['{}'.format(name)])
+    plt.plot(history.history['val_{}'.format(name)])
+    
+    plt.title('Model {}'.format(name))
+    plt.ylabel(metric)
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Val'], loc='upper left')
+    plt.savefig('{}{}.png'.format(save_path,name))
+
+def plot_history(history, save_path):
+    # plot total loss
+    plot_param(history, save_path, param='', metric='loss')
+    # plot losses
+    plot_param(history, save_path, param='output_grapheme', metric='loss')
+    plot_param(history, save_path, param='output_vowel', metric='loss')
+    plot_param(history, save_path, param='output_consonant', metric='loss')
+    # plot accuracies
+    plot_param(history, save_path, param='output_grapheme', metric='acc')
+    plot_param(history, save_path, param='output_vowel', metric='acc')
+    plot_param(history, save_path, param='output_consonant', metric='acc')
+    
+
+# PARAMETERS
+# ==================================================================
 DATASET_PATH = '/home/rauf/datasets/bengali/'
 HEIGHT = 137
 WIDTH = 236
 SIZE = 128
+
+n_epochs = 1
+batch_size = 64
+
+work_dirs_path = 'work_dirs/'
+
+backbone_name = 'efficientnet-b5'
+config_name = backbone_name
+weights_save_path = os.path.join(work_dirs_path, config_name, 'weights/')
+logs_save_path = os.path.join(work_dirs_path, config_name, 'logs/')
+plots_save_path = os.path.join(work_dirs_path, config_name, 'plots/')
+os.makedirs(work_dirs_path, exist_ok=True)
+os.makedirs(weights_save_path, exist_ok=True)
+os.makedirs(logs_save_path, exist_ok=True)
+os.makedirs(plots_save_path, exist_ok=True)
+
+n_classes_grapheme = 168
+n_classes_vowel = 11
+n_classes_consonant = 7
+# optimizer = optimizers.rmsprop(lr = 0.0001, decay = 1e-6)
+optimizer = RAdam(learning_rate=0.001)
+
+# ==================================================================
+
 
 train_csv_file = os.path.join(DATASET_PATH, 'train.csv')
 full_df = pd.read_csv(train_csv_file)
@@ -39,9 +89,6 @@ images_path_test = os.path.join(DATASET_PATH, 'pngs/','test')
 
 datagen = ImageDataGenerator(rescale=1./255.)
 test_datagen = ImageDataGenerator(rescale=1./255.)
-
-n_epochs = 1000
-batch_size = 64
 
 train_generator = datagen.flow_from_dataframe(dataframe=train_df,
                                             directory=images_path_train,
@@ -65,25 +112,31 @@ val_generator = datagen.flow_from_dataframe(dataframe=val_df,
                                             class_mode="other",
                                             target_size=(SIZE,SIZE))
 
-backbone_name = 'efficientnetB5'
-# backbone_name = 'seresnet50'
-weights_save_path = 'weights/'
-logs_save_path = 'logs/'
-plots_save_path = 'plots/'
-# BaseModel, preprocess_input = Classifiers.get(backbone_name)
-# base_model = BaseModel(input_shape=(128, 128, 1), weights=None, include_top=False)
-base_model = efn.EfficientNetB5(input_shape=(128, 128, 1), weights=None, include_top=False)
+if backbone_name.startswith('efficientnet'):
+    efficientnet_models = {
+        'efficientnet-b0': efn.EfficientNetB0,
+        'efficientnet-b1': efn.EfficientNetB1,
+        'efficientnet-b2': efn.EfficientNetB2,
+        'efficientnet-b3': efn.EfficientNetB3,
+        'efficientnet-b4': efn.EfficientNetB4,
+        'efficientnet-b5': efn.EfficientNetB5,
+        'efficientnet-b6': efn.EfficientNetB6,
+        'efficientnet-b7': efn.EfficientNetB7,
+    }
+    Efficientnet_model = efficientnet_models[backbone_name]
+    base_model = Efficientnet_model(input_shape=(128, 128, 1), weights=None, include_top=False)
+else:
+    BaseModel, preprocess_input = Classifiers.get(backbone_name)
+    base_model = BaseModel(input_shape=(128, 128, 1), weights=None, include_top=False)
 
-n_classes_grapheme = 168
-n_classes_vowel = 11
-n_classes_consonant = 7
-# optimizer = optimizers.rmsprop(lr = 0.0001, decay = 1e-6)
-optimizer = RAdam(learning_rate=0.001)
 
 # x0 = keras.layers.Flatten()(base_model.output)
 x0 = keras.layers.GlobalAveragePooling2D()(base_model.output)
-print(x0)
-x = keras.layers.Dense(256, activation='relu')(x0)
+
+x = keras.layers.Dense(512, activation='relu')(x0)
+x = keras.layers.BatchNormalization()(x)
+x = keras.layers.Dropout(rate=0.5)(x)
+x = keras.layers.Dense(256, activation='relu')(x)
 x = keras.layers.BatchNormalization()(x)
 x = keras.layers.Dropout(rate=0.5)(x)
 x = keras.layers.Dense(128, activation='relu')(x)
@@ -96,7 +149,10 @@ output_grapheme = keras.layers.Dense(n_classes_grapheme,
                                      activation='softmax', 
                                      name = 'output_grapheme')(x)
 
-x = keras.layers.Dense(256, activation='relu')(x0)
+x = keras.layers.Dense(512, activation='relu')(x0)
+x = keras.layers.BatchNormalization()(x)
+x = keras.layers.Dropout(rate=0.5)(x)
+x = keras.layers.Dense(256, activation='relu')(x)
 x = keras.layers.BatchNormalization()(x)
 x = keras.layers.Dropout(rate=0.5)(x)
 x = keras.layers.Dense(128, activation='relu')(x)
@@ -109,7 +165,10 @@ output_vowel = keras.layers.Dense(n_classes_vowel,
                                   activation='softmax', 
                                   name = 'output_vowel')(x)
 
-x = keras.layers.Dense(256, activation='relu')(x0)
+x = keras.layers.Dense(512, activation='relu')(x0)
+x = keras.layers.BatchNormalization()(x)
+x = keras.layers.Dropout(rate=0.5)(x)
+x = keras.layers.Dense(256, activation='relu')(x)
 x = keras.layers.BatchNormalization()(x)
 x = keras.layers.Dropout(rate=0.5)(x)
 x = keras.layers.Dense(128, activation='relu')(x)
@@ -129,12 +188,12 @@ model.summary()
 model.compile(optimizer=optimizer, loss = {'output_grapheme':'categorical_crossentropy', 
                                          'output_vowel':'categorical_crossentropy',
                                          'output_consonant':'categorical_crossentropy'},
-                                 loss_weights = {'output_grapheme': 1,
-                                                 'output_vowel': 0.2,
-                                                 'output_consonant': 0.2}, 
+                                 loss_weights = {'output_grapheme': 0.09,
+                                                 'output_vowel': 0.06,
+                                                 'output_consonant': 0.04}, 
                                  metrics=['accuracy'])
 
-# keras.utils.plot_model(model, '{}{}_model.png'.format(plots_save_path, backbone_name))
+keras.utils.plot_model(model, '{}{}_model.png'.format(plots_save_path, backbone_name))
 
 STEP_SIZE_TRAIN=train_generator.n//train_generator.batch_size
 STEP_SIZE_VALID=val_generator.n//val_generator.batch_size
@@ -157,10 +216,13 @@ callbacks = [ModelCheckpoint(checkpoints_save_name ,
 
 # model.load_weights(checkpoints_save_name)
 
-model.fit_generator(generator=generator_wrapper(train_generator),
+history = model.fit_generator(generator=generator_wrapper(train_generator),
                     steps_per_epoch=STEP_SIZE_TRAIN,
                     validation_data=generator_wrapper(val_generator),
                     validation_steps=STEP_SIZE_VALID,
                     epochs=n_epochs,
                     verbose=1,
                     callbacks=callbacks)
+
+
+plot_history(history, plots_save_path)
